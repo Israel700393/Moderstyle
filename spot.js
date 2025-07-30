3a3ccfdf7fdf91d48a1ad647e297acd80c4a2e40
Israel700393 <israelrangel021223@gmail.com>
2025-07-30 15:55:22 -0300

    5225252

--- spot.js
+++ spot.js
@@ -16,3 +16,23 @@ document.querySelectorAll('.add-cart').forEach(button => {
   }); 
 });
 
+var lastScrollTop = 0;
+var backgroundImages = $('.backgroundImage'); 
+
+$(window).scroll(function(e){
+  var st = $(this).scrollTop();
+  var ah = $(this).height();
+  backgroundImages.each(function(i){
+    var img = $(this);
+    var pos = img.position().top;
+    var hei = img.height();
+    if ((st + ah) > pos && st < (pos + hei)){
+      var p = ((pos - st)/ah) + 0.25;
+      if(i == 1) console.log(p);
+      img.css('background-position', '50%'+(p*100)+'%');
+    }
+  });
+  lastScrollTop = st;
+});
+
+$(window).scroll();
